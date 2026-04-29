0b929f7bbf1921a3d4730688fb27dabbb34d7226
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2026-04-29 12:49:14 -0600

    Cleanup mentores ids

--- mentores-25-26-ids.R
+++ mentores-25-26-ids.R
@@ -1,6 +1,7 @@
-library(tidyverse)
 library(janitor)
+library(openxlsx)
 library(stringi)
+library(tidyverse)
 
 lb_path = "data/excel/LB-Mentores-JAT-25-26.csv"
 lf_path = "data/excel/LF-Mentores-JAT-25-26.csv"
@@ -18,6 +19,56 @@ cols_id <- c(
     "sede" = "selecciona_tu_sede"
 )
 
+reemplazos_nombres <-
+    tibble(
+        from = c(
+            "Aidee Joselin Quiajda Dominguez",
+            "Allisson Fernanda Garcia Castro",
+            "Analia Galicia Serrano",
+            "Angel Morin Villarreal",
+            "Christian Abdiel Jaramillo Medellin.",
+            "Devany Lizath Zuniga Arriaga",
+            "Yosgard Garcia Hernandez",
+            "Vanesa Chavez Juarez",
+            "Gerardo Martinez Vallejo",
+            "Lidia Fernandez Marquez",
+            "Luz Anayetzy Orgaz Diaz Diaz",
+            "Citlali Antonio Hernandez",
+            "Dayra Natividad Leos",
+            "Karen Viri Hernandez",
+            "Maria De Jesus Martinez Martinez",
+            "Ricardo Martinez Martinez",
+            "Santiago Escamilla Araujo",
+            "Sarahi Robledo Iracheta",
+            "Darynka Martinez Gonzalez",
+            "Dulce Julian Mendez",
+            "Ricardo Martinez Canamar"
+        ),
+        to = c(
+            "Aidee Joselin Quijada Dominguez",
+            "Allison Fernanda Garcia Castro",
+            "Analia Rubi Galicia Serrano",
+            "Angel Enrique Morin Villarreal",
+            "Christian Abdiel Jaramillo Medellin",
+            "Devany Lizeth Zuniga Arriaga",
+            "Yosgard Said Garcia Hernandez",
+            "Vanesa Gabriela Chavez Juarez",
+            "Gerardo Israel Martinez Vallejo",
+            "Lidia Viridiana Fernandez Marquez",
+            "Luz Anayetzy Orgaz Diaz",
+            "Citlali Magdale Antonio Hernandez",
+            "Dayra Astrid Natividad Leos",
+            "Karen Viridiana Sandoval Hernandez",
+            "Maria De Jesus Martinez Escamilla",
+            "Ricardo Martinez Canamar",
+            "Alexander Santiago Escamilla Araujo",
+            "Maria Sarahi Robledo Iracheta",
+            "Isis Darynka Martinez Gonzalez",
+            "Dulce Silvana Julian Mendez",
+            "Oliver Ricardo Martinez Canamar"
+        )
+    )
+
 text_cleaning <- function(m_df) {
     m_df_cleaned <- m_df %>%
         mutate(across(
@@ -26,7 +77,7 @@ text_cleaning <- function(m_df) {
                 stringr::str_squish()
         )) %>%
         mutate(
-            across(any_of(c("email")), ~ stringr::str_to_lower(.x)),
+            across(any_of(c("id", "email")), ~ stringr::str_to_lower(.x)),
             celular = as.character(celular)
         ) %>%
         filter(
@@ -47,6 +98,13 @@ text_cleaning <- function(m_df) {
                 "latin-ascii"
             )
         ) %>%
+        mutate(
+            nombre_completo = dplyr::replace_values(
+                nombre_completo,
+                from = reemplazos_nombres$from,
+                to = reemplazos_nombres$to
+            )
+        ) %>%
         distinct()
     return(m_df_cleaned)
 }
@@ -54,9 +112,40 @@ text_cleaning <- function(m_df) {
 lb_mentores <- read_csv(lb_path) %>%
     janitor::clean_names() %>%
     select(all_of(cols_id)) %>%
-    text_cleaning()
+    text_cleaning() %>%
+    mutate(inicia = TRUE)
 
 lf_mentores <- read_csv(lf_path) %>%
     janitor::clean_names() %>%
     select("id", all_of(cols_id)) %>%
-    text_cleaning()
+    text_cleaning() %>%
+    mutate(concluye = TRUE)
+
+
+mentores_ids <-
+    full_join(
+        select(lb_mentores, -any_of(c("celular", "email", "curp"))),
+        select(lf_mentores, -any_of(c("celular", "email", "curp"))),
+        by = "nombre_completo"
+    ) %>%
+    mutate(
+        edad = coalesce(edad.x, edad.y),
+        sexo = coalesce(sexo.x, sexo.y),
+        estado = coalesce(estado.x, estado.y),
+        sede = coalesce(sede.x, sede.y)
+    ) %>%
+    select(-matches("\\.(x|y)$")) %>%
+    mutate(
+        estado = stringr::str_replace(estado, "De", "de"),
+        across(all_of(c("inicia", "concluye")), ~ ifelse(is.na(.x), FALSE, .x))
+    ) %>%
+    mutate(
+        status = case_when(
+            inicia & concluye ~ "Graduado",
+            inicia & !concluye ~ "No graduado",
+            !inicia & concluye ~ "Graduado, no inició"
+        )
+    ) %>%
+    distinct()
+
+write_csv(mentores_ids, "output/seguimiento/mentores-25-25-ids.csv")
