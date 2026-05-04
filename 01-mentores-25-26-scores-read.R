a42d0ab7b12827399b1aa564d694c9093dc70917
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2026-05-04 11:52:25 -0600

    Línea base 25-26

--- 01-mentores-25-26-scores-read.R
+++ 01-mentores-25-26-scores-read.R
@@ -3,57 +3,7 @@ library(janitor)
 library(readxl)
 library(tidyverse)
 
-reemplazos_nombres <-
-    tibble(
-        from = c(
-            "Aidee Joselin Quiajda Dominguez",
-            "Allisson Fernanda Garcia Castro",
-            "Analia Galicia Serrano",
-            "Angel Morin Villarreal",
-            "Christian Abdiel Jaramillo Medellin.",
-            "Devany Lizath Zuniga Arriaga",
-            "Yosgard Garcia Hernandez",
-            "Vanesa Chavez Juarez",
-            "Gerardo Martinez Vallejo",
-            "Lidia Fernandez Marquez",
-            "Luz Anayetzy Orgaz Diaz Diaz",
-            "Citlali Antonio Hernandez",
-            "Dayra Natividad Leos",
-            "Karen Viri Hernandez",
-            "Maria De Jesus Martinez Martinez",
-            "Ricardo Martinez Martinez",
-            "Santiago Escamilla Araujo",
-            "Sarahi Robledo Iracheta",
-            "Darynka Martinez Gonzalez",
-            "Dulce Julian Mendez",
-            "Ricardo Martinez Canamar"
-        ),
-        to = c(
-            "Aidee Joselin Quijada Dominguez",
-            "Allison Fernanda Garcia Castro",
-            "Analia Rubi Galicia Serrano",
-            "Angel Enrique Morin Villarreal",
-            "Christian Abdiel Jaramillo Medellin",
-            "Devany Lizeth Zuniga Arriaga",
-            "Yosgard Said Garcia Hernandez",
-            "Vanesa Gabriela Chavez Juarez",
-            "Gerardo Israel Martinez Vallejo",
-            "Lidia Viridiana Fernandez Marquez",
-            "Luz Anayetzy Orgaz Diaz",
-            "Citlali Magdale Antonio Hernandez",
-            "Dayra Astrid Natividad Leos",
-            "Karen Viridiana Sandoval Hernandez",
-            "Maria De Jesus Martinez Escamilla",
-            "Ricardo Martinez Canamar",
-            "Alexander Santiago Escamilla Araujo",
-            "Maria Sarahi Robledo Iracheta",
-            "Isis Darynka Martinez Gonzalez",
-            "Dulce Silvana Julian Mendez",
-            "Oliver Ricardo Martinez Canamar"
-        )
-    )
-
-url_id_lb <- "1CzxR6Gn8MOsFnbQ871tIHoP1pn0v8JMqXH7zhBZXL1k"
+url_id_lb <- "1AB4ik3ccD0mHRSsroIcXied6oEvLck0Nv44eicIjsg8"
 url_id_lf <- "1QWOZi7U_kg3ELUIvoQI35iso7ZcGKZGj6FPF0cSZ-38"
 
 path_lb <- "data/excel/LB mentores 25-26.xlsx"
@@ -61,10 +11,12 @@ path_lf <- "data/excel/LF mentores 25-26.xlsx"
 
 googledrive::drive_download(
     file = googledrive::as_id(url_id_lb),
-    path = path_lb
+    path = path_lb,
+    overwrite = TRUE
 )
 
 googledrive::drive_download(
     file = googledrive::as_id(url_id_lf),
-    path = path_lf
+    path = path_lf,
+    overwrite = TRUE
 )
--- 02-mentores-25-26-scores-analysis.R
+++ 02-mentores-25-26-scores-analysis.R
@@ -15,11 +15,23 @@ if (!dir.exists(output_path)) {
 path_lb <- "data/excel/LB mentores 25-26.xlsx"
 path_lf <- "data/excel/LF mentores 25-26.xlsx"
 
-lb <- readxl::read_excel(path_lf) %>%
-    janitor::clean_names()
+lb <- readxl::read_excel(path_lb) %>%
+    janitor::clean_names() %>%
+    filter(stringr::str_length(q4_correo_electronico) > 1)
 
 lf <- readxl::read_excel(path_lf) %>%
     janitor::clean_names()
+
+lb_cats <-
+    lb %>%
+    select(
+        "respondent_id",
+        "edad" = "q5_edad_anos_cumplidos",
+        "sexo" = "q6_sexo",
+        "estado" = "q10_estado_donde_vives",
+        "sede" = "q11_selecciona_tu_sede",
+    ) %>%
+    mutate("nacional" = "Nacional")
 
 lf_cats <-
     lf %>%
@@ -31,6 +43,74 @@ lf_cats <-
         "sede" = "q12_selecciona_tu_sede",
     ) %>%
     mutate("nacional" = "Nacional")
+
+group_list <- list(
+    "nacional" = "nacional",
+    "edad" = "edad",
+    "estado" = "estado",
+    "sede" = "sede"
+)
+
+items_to_reverse <- c(
+    "Teamwork_07",
+    "Empathy_02",
+    "Empathy_03",
+    "Empathy_04",
+    "Decision_06",
+    "Decision_07",
+    "Decision_08",
+    "Decision_09"
+)
+
+reemplazos_nombres <- tibble(
+        from = c(
+            "Aidee Joselin Quiajda Dominguez",
+            "Allisson Fernanda Garcia Castro",
+            "Analia Galicia Serrano",
+            "Angel Morin Villarreal",
+            "Christian Abdiel Jaramillo Medellin.",
+            "Devany Lizath Zuniga Arriaga",
+            "Yosgard Garcia Hernandez",
+            "Vanesa Chavez Juarez",
+            "Gerardo Martinez Vallejo",
+            "Lidia Fernandez Marquez",
+            "Luz Anayetzy Orgaz Diaz Diaz",
+            "Citlali Antonio Hernandez",
+            "Dayra Natividad Leos",
+            "Karen Viri Hernandez",
+            "Maria De Jesus Martinez Martinez",
+            "Ricardo Martinez Martinez",
+            "Santiago Escamilla Araujo",
+            "Sarahi Robledo Iracheta",
+            "Darynka Martinez Gonzalez",
+            "Dulce Julian Mendez",
+            "Ricardo Martinez Canamar"
+        ),
+        to = c(
+            "Aidee Joselin Quijada Dominguez",
+            "Allison Fernanda Garcia Castro",
+            "Analia Rubi Galicia Serrano",
+            "Angel Enrique Morin Villarreal",
+            "Christian Abdiel Jaramillo Medellin",
+            "Devany Lizeth Zuniga Arriaga",
+            "Yosgard Said Garcia Hernandez",
+            "Vanesa Gabriela Chavez Juarez",
+            "Gerardo Israel Martinez Vallejo",
+            "Lidia Viridiana Fernandez Marquez",
+            "Luz Anayetzy Orgaz Diaz",
+            "Citlali Magdale Antonio Hernandez",
+            "Dayra Astrid Natividad Leos",
+            "Karen Viridiana Sandoval Hernandez",
+            "Maria De Jesus Martinez Escamilla",
+            "Ricardo Martinez Canamar",
+            "Alexander Santiago Escamilla Araujo",
+            "Maria Sarahi Robledo Iracheta",
+            "Isis Darynka Martinez Gonzalez",
+            "Dulce Silvana Julian Mendez",
+            "Oliver Ricardo Martinez Canamar"
+        )
+    )
+
 
 # Functions ----
 recode_answer <- function(wide_question) {
@@ -47,18 +127,25 @@ recode_answer <- function(wide_question) {
     return(recoded_answer)
 }
 
+reverse_scale <- function(scale_values, max_value = 4) {
+    reverse_values <- (max_value + 1) - scale_values
+    return(reverse_values)
+}
+
 widen_question <- function(
+    df_linea,
     question_prefix,
     question_label,
     number_of_questions
 ) {
     n_questions <- 1:number_of_questions
     wide_question <-
-        select(lf, c("respondent_id"), starts_with(question_prefix)) |>
+        select(df_linea, c("respondent_id"), starts_with(question_prefix)) |>
         separate_wider_delim(
             starts_with(question_prefix),
             delim = "|",
-            names = paste0(question_prefix, "_", n_questions)
+            names = paste0(question_prefix, "_", n_questions),
+            too_few = "align_start"
         ) %>%
         pivot_longer(
             cols = starts_with(question_prefix),
@@ -68,12 +155,14 @@ widen_question <- function(
         separate_wider_delim(
             "question_n",
             delim = "_",
-            names = c("scale", "number")
+            names = c("scale", "number"),
+            too_few = "align_start"
         ) %>%
         separate_wider_delim(
             "question_text",
             delim = ":",
-            names = c("question", "answer")
+            names = c("question", "answer"),
+            too_few = "align_start"
         ) %>%
         mutate(
             across(everything(), ~ stringr::str_squish(.x)),
@@ -90,6 +179,7 @@ get_itemstats <- function(wide_question) {
         tidyr::unite("item", c("scale", "number")) %>%
         pivot_wider(names_from = "item", values_from = "answer") %>%
         select(-c("respondent_id")) %>%
+        mutate(across(any_of(items_to_reverse), ~ reverse_scale(.x))) %>%
         mirt::itemstats()
 
     alpha_value = question_stats$overall$alpha
@@ -99,7 +189,7 @@ get_itemstats <- function(wide_question) {
         question_stats$proportions
     ) %>%
         mutate(
-    alpha = alpha_value,
+            alpha = alpha_value,
             across(where(is.numeric), ~ round(.x, 2)),
         ) %>%
         rownames_to_column("item") %>%
@@ -152,52 +242,95 @@ scale_summary <- function(scale_df, data_cats, group) {
 }
 
 # Analysis ----
-agnc_1 <- widen_question("q14", "Agency", 5)
-agnc_2 <- widen_question("q15", "Agency", 5)
-team <- widen_question("q29", "Teamwork", 9)
-lead <- widen_question("q30", "Leadership", 7)
-empt <- widen_question("q31", "Empathy", 5)
+lb_ag_1 <- widen_question(lb, "q13", "Agency", 5)
+lb_ag_2 <- widen_question(lb, "q14", "Agency", 5)
+lb_team <- widen_question(lb, "q21", "Teamwork", 9)
+lb_lead <- widen_question(lb, "q22", "Leadership", 7)
+lb_empt <- widen_question(lb, "q23", "Empathy", 5)
+#widen_question("q24", "Decision", 9)
+
+lf_ag_1 <- widen_question(lf, "q14", "Agency", 5)
+lf_ag_2 <- widen_question(lf, "q15", "Agency", 5)
+lf_team <- widen_question(lf, "q29", "Teamwork", 9)
+lf_lead <- widen_question(lf, "q30", "Leadership", 7)
+lf_empt <- widen_question(lf, "q31", "Empathy", 5)
 #widen_question("q32", "Decision", 9)
 
-agnc <-
-    agnc_2 %>%
+lb_agnc <-
+    lb_ag_2 %>%
     mutate(number = as.character(as.numeric(number) + 5)) %>%
-    bind_rows(agnc_1) %>%
+    bind_rows(lb_ag_1) %>%
     arrange(respondent_id, number)
-agnc_itemstats <- get_itemstats(agnc)
-team_itemstats <- get_itemstats(team)
-lead_itemstats <- get_itemstats(lead)
-empt_itemstats <- get_itemstats(empt)
+lb_agnc_itemstats <- get_itemstats(lb_agnc)
+lb_team_itemstats <- get_itemstats(lb_team)
+lb_lead_itemstats <- get_itemstats(lb_lead)
+lb_empt_itemstats <- get_itemstats(lb_empt)
 
-itemstats_list <- list(
-    "agnc" = agnc_itemstats,
-    "team" = team_itemstats,
-    "lead" = lead_itemstats,
-    "empt" = empt_itemstats
+lf_agnc <-
+    lf_ag_2 %>%
+    mutate(number = as.character(as.numeric(number) + 5)) %>%
+    bind_rows(lf_ag_1) %>%
+    arrange(respondent_id, number)
+lf_agnc_itemstats <- get_itemstats(lf_agnc)
+lf_team_itemstats <- get_itemstats(lf_team)
+lf_lead_itemstats <- get_itemstats(lf_lead)
+lf_empt_itemstats <- get_itemstats(lf_empt)
+
+# Grouped
+lb_itemstats_list <- list(
+    "agnc" = lb_agnc_itemstats,
+    "team" = lb_team_itemstats,
+    "lead" = lb_lead_itemstats,
+    "empt" = lb_empt_itemstats
 )
 
-scale_list <- list(
-    "agnc" = agnc,
-    "team" = team,
-    "lead" = lead,
-    "empt" = empt
+lb_scale_list <- list(
+    "agnc" = lb_agnc,
+    "team" = lb_team,
+    "lead" = lb_lead,
+    "empt" = lb_empt
 )
 
-group_list <- list(
-    "nacional" = "nacional",
-    "edad" = "edad",
-    "estado" = "estado",
-    "sede" = "sede"
+lf_itemstats_list <- list(
+    "agnc" = lf_agnc_itemstats,
+    "team" = lf_team_itemstats,
+    "lead" = lf_lead_itemstats,
+    "empt" = lf_empt_itemstats
 )
 
-scale_summaries <-
-    map(scale_list, function(scale_x) {
+lf_scale_list <- list(
+    "agnc" = lf_agnc,
+    "team" = lf_team,
+    "lead" = lf_lead,
+    "empt" = lf_empt
+)
+
+lb_scale_summaries <-
+    map(lb_scale_list, function(scale_x) {
+        map(group_list, function(group_x) {
+            scale_summary(scale_x, lb_cats, group_x)
+        })
+    })
+
+lf_scale_summaries <-
+    map(lf_scale_list, function(scale_x) {
         map(group_list, function(group_x) {
             scale_summary(scale_x, lf_cats, group_x)
         })
     })
 
 # Export ----
+write_rds(scale_list, paste0(output_path, "/lb-scales-mentores-25-26.rds"))
+write_rds(
+    itemstats_list,
+    paste0(output_path, "/lb-psychometrics-mentores-25-26.rds")
+)
+write_rds(
+    scale_summaries,
+    paste0(output_path, "/lb-statistics-mentores-25-26.rds")
+)
+
+
 write_rds(scale_list, paste0(output_path, "/lf-scales-mentores-25-26.rds"))
 write_rds(
     itemstats_list,
